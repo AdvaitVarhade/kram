48778f460a3967c6bee8d11014363425f88001bf
Mark <84480133+re-mark-able@users.noreply.github.com>
2026-07-30 01:47:37 +0000

    rename movie/show command to watch and boardgame command to play. Prep DB for new functions

--- commands/play.js
+++ commands/play.js
@@ -10,8 +10,8 @@ const { XMLParser } = require("fast-xml-parser");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("bg")
-    .setDescription("Search for a board game")
+    .setName("play")
+    .setDescription("Search for a board game, rate it or queue it.")
     .addStringOption((option) =>
       option
         .setName("search_string")
