373dd8cdef24c29290bd5bc940812206954128db
Mark <84480133+re-mark-able@users.noreply.github.com>
2026-07-30 03:08:01 +0000

    lint fixes

--- utils/logToChannel.js
+++ utils/logToChannel.js
@@ -3,35 +3,40 @@ const { channels, defaultColour } = require("./config");
 const logger = require("./logger");
 
 module.exports = (msg, client, severity = "info") => {
-    const severityColours = {
-        default: defaultColour,
-        success: Colors.Green,
-        info: Colors.Blue,
-        warn: Colors.Yellow,
-        danger: Colors.Red
-    }
-    const severityIcons = {
-        default: "⚡",
-        success: "✅",
-        info: "ℹ️",
-        warn: "⚠️",
-        danger: "🚨"
-    }
-    const logContainer = new ContainerBuilder()
-        .setAccentColor(!severityColours[severity] ? severityColours.default : severityColours[severity])
-        .addTextDisplayComponents((textDisplay) => textDisplay
-            .setContent(`${!severityIcons[severity] ? severityIcons.default : severityIcons[severity]}\n\`\`\`${msg}\`\`\``)
-        );
-    
-    const logChannel = client.channels.cache.get(channels.logChannel);
+  const severityColours = {
+    default: defaultColour,
+    success: Colors.Green,
+    info: Colors.Blue,
+    warn: Colors.Yellow,
+    danger: Colors.Red,
+  };
+  const severityIcons = {
+    default: "⚡",
+    success: "✅",
+    info: "ℹ️",
+    warn: "⚠️",
+    danger: "🚨",
+  };
+  const logContainer = new ContainerBuilder()
+    .setAccentColor(
+      !severityColours[severity]
+        ? severityColours.default
+        : severityColours[severity],
+    )
+    .addTextDisplayComponents((textDisplay) =>
+      textDisplay.setContent(
+        `${!severityIcons[severity] ? severityIcons.default : severityIcons[severity]}\n\`\`\`${msg}\`\`\``,
+      ),
+    );
 
-    try {
-        logChannel.send({
-            components: [logContainer],
-            flags: MessageFlags.IsComponentsV2
-        })
-    } catch (e) {
-        logger.error(e, "Log channel message error");
-    }
+  const logChannel = client.channels.cache.get(channels.logChannel);
 
-}
+  try {
+    logChannel.send({
+      components: [logContainer],
+      flags: MessageFlags.IsComponentsV2,
+    });
+  } catch (e) {
+    logger.error(e, "Log channel message error");
+  }
+};
